fc215e147e3e60165f5e58092089d503b23ae7dd
kung <lwkung@gmail.com>
2018-10-16 21:50:45 +0800

    2th

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -1,13 +1,50 @@
 package com.home.currency;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edNtd;
+    private Button btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+    }
+
+    private void findViews() {
+        edNtd = findViewById(R.id.ntd);
+        btnGo = findViewById(R.id.button);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ntd = edNtd.getText().toString();
+                if (ntd.equals("")) {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Problem")
+                            .setMessage("Please enter your NTD amount")
+                            .setPositiveButton("OK", null)
+                            .show();
+                } else {
+                    String result = String.format(Locale.ENGLISH, "%.4f", Integer.parseInt(ntd) / 30.9);
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Result")
+                            .setMessage("USD is " + result)
+                            .setPositiveButton("OK", null)
+                            .show();
+                }
+            }
+        });
     }
 }
